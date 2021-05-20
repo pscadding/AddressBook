6f6c1b335c0eef5fabb8106bb08c7e7118da2ea2
Philip Scadding <poscadding@hotmail.co.uk>
2021-05-20 15:48:23 +0100

    Added comments and cleared out broken tests.

--- src/app/address-create/address-create.component.spec.ts
+++ src/app/address-create/address-create.component.spec.ts
@@ -1,24 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AddressCreateComponent } from './address-create.component';
 
-describe('AddressCreateComponent', () => {
-  let component: AddressCreateComponent;
-  let fixture: ComponentFixture<AddressCreateComponent>;
-
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [AddressCreateComponent]
     }).compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddressCreateComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AddressCreateComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 });
--- src/app/address.service.ts
+++ src/app/address.service.ts
@@ -42,39 +42,21 @@ export class AddressService {
   }
 
   /**
-   * Filters the provided list of addresses based on the searchString.
-   * The search string is applied in a caseinsensitive match again
-   *  both first and last names on the address.
-   * @param addressesToFilter an array of AddressItems
-   * @returns a filtered array of AddressItems
+   * Exports the addresses stored in this service, by passing them to electron to handle.
    */
-  private filterAddresses(addressesToFilter: AddressItem[]): AddressItem[] {
-    // Perform a case insensitive match against the start of a string.
-    const re = new RegExp(`^${this.searchString}`, 'gmi');
-    const filtered = addressesToFilter.filter((address) => {
-      if (address.firstName.match(re)) return true;
-      if (address.lastName.match(re)) return true;
-      return false;
-    });
-    return filtered;
+  exportAddresses(): void {
+    // TODO: Check that the data can be serialized and provide a method of choosing an output path.
+    // TODO: should return success of failure. 
+    if (this._ipc) {
+      this._ipc.send('exportAddresses', this.addresses);
+    }
   }
 
-  private sortAddresses(addressesToSort: AddressItem[]): AddressItem[] {
-    const AddressSort = (a: AddressItem, b: AddressItem): number => {
-      // TODO: This would need to be more complex, but for now everything is alphabetical
-      //   and there are only two options for sorting by first name and last name.
-      const paramName =
-        this.sortChoice == SortOption.firstNameAlpha ? 'firstName' : 'lastName';
-      // Sort Alphabetically and case insensitively.
-      const aParam = a[paramName].toLowerCase();
-      const bParam = b[paramName].toLowerCase();
-      if (aParam < bParam) return -1;
-      if (aParam > bParam) return 1;
-      return 0;
-    };
-    return addressesToSort.sort(AddressSort);
-  }
-
+  /**
+   * Returns an a sorted and filtered array of AddressItems.
+   * Sorting and Filtering options are set via the searchString, and sortChoice service properties.
+   * @returns Array of AddressItems
+   */
   getAddresses(): AddressItem[] {
     const adrs = Object.values(this.addresses);
     const filteredAdrs = this.filterAddresses(adrs);
@@ -95,7 +77,7 @@ export class AddressService {
 
   /**
    * Adds an address to the address store.
-   * @param address The addres (AddressItem) to add to the list of addresses
+   * @param address The address (AddressItem) to add to the list of addresses
    * @returns boolean stating whether the address was added or not.
    */
   addAddress(address: PlaceholderAddressItem) {
@@ -112,22 +94,62 @@ export class AddressService {
     return true;
   }
 
+  /**
+   * Removed the passed AddressItem from the service's store of addresses.
+   * @param address AddressItem to remove.
+   */
   removeAddress(address: AddressItem) {
     delete this.addresses[address.id];
   }
 
+  /**
+   * Filters the provided list of addresses based on the searchString.
+   * The search string is applied in a case insensitive match again
+   *  both first and last names on the address.
+   * @param addressesToFilter an array of AddressItems
+   * @returns a filtered array of AddressItems
+   */
+   private filterAddresses(addressesToFilter: AddressItem[]): AddressItem[] {
+    // Perform a case insensitive match against the start of a string.
+    const re = new RegExp(`^${this.searchString}`, 'gmi');
+    const filtered = addressesToFilter.filter((address) => {
+      if (address.firstName.match(re)) return true;
+      if (address.lastName.match(re)) return true;
+      return false;
+    });
+    return filtered;
+  }
+
+  /**
+   * Given an array of addresses, it will sort them in accordance with the set sort choice.
+   * @param addressesToSort Array of AddressItems
+   * @returns A sorted array of addresses.
+   */
+  private sortAddresses(addressesToSort: AddressItem[]): AddressItem[] {
+    const AddressSort = (a: AddressItem, b: AddressItem): number => {
+      // TODO: This would need to be more complex, but for now everything is alphabetical
+      //   and there are only two options for sorting by first name and last name.
+      const paramName =
+        this.sortChoice == SortOption.firstNameAlpha ? 'firstName' : 'lastName';
+      // Sort Alphabetically and case insensitively.
+      const aParam = a[paramName].toLowerCase();
+      const bParam = b[paramName].toLowerCase();
+      if (aParam < bParam) return -1;
+      if (aParam > bParam) return 1;
+      return 0;
+    };
+    return addressesToSort.sort(AddressSort);
+  }
+
+  /**
+   * Generates a new ID for a potential address. This is very much a placeholder method.
+   * @returns 
+   */
   private generateID(): number {
     // This is just rough code to find an id number that hasn't been used.
     // I wouldn't expect it to work like this.
     const ids = Object.keys(this.addresses).map(Number);
     // provide 0 as well incase we don't have any ids currently.
     return Math.max(...ids, 0) + 1;
-  }
-
-  exportAddresses() {
-    // TODO: Check that the data can be serialized and provide a method of chosing an output path.
-    if (this._ipc) {
-      this._ipc.send('exportAddresses', this.addresses);
-    }
   }
 }
